a431eb63df52b88b1d0e44a58174dc0c16a70c74
gregorymark254 <gregorymark254@gmail.com>
2023-12-07 09:56:41 +0300

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+import json
+from os.path import exists
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''returns the dictionary __objects'''
+        pass
+
+    def new(self, obj):
+        '''sets in __objects the obj with key <obj class name>.id'''
+        pass
+
+    def save(self):
+        '''serializes __objects to the JSON file (path: __file_path)'''
+        pass
+
+    def reload(self):
+        ''' 
+        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; 
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+        '''
+        pass
