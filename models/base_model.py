18837d555c1edf461552cfd68c3a57d46788d0b6
gregorymark254 <gregorymark254@gmail.com>
2023-12-07 13:58:15 +0300

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -25,12 +26,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """
         Updates updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
