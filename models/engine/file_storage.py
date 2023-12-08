4af518f3686b32940f52a4f84c2c06f773f3af55
gregorymark254 <gregorymark254@gmail.com>
2023-12-08 20:56:46 +0300

    filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,7 +41,24 @@ class FileStorage:
 
     def classes(self):
         '''Returns a list of class names.'''
-        return [key.split('.')[0] for key in self.__objects.keys()]
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review,
+        }
+        return classes
 
 
 # Creating an instance for file storage
