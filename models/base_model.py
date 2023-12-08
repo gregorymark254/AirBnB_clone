32bf7c70d1f64fe82ca246c8e15030faeb0bc408
gregorymark254 <gregorymark254@gmail.com>
2023-12-08 09:57:47 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -24,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         '''Returns a string representation of the BaseModel instance.'''
@@ -33,6 +35,7 @@ class BaseModel:
     def save(self):
         '''Updates updated_at with the current datetime.'''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         '''returns a dictionary containing all keys/values of __dict__'''
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+import json
+from os.path import isfile
+
+
+class FileStorage:
+    '''Serializes instances to a JSON file and deserializes JSON file to instances.'''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''returns the dictionary __objects'''
+        return self.__objects
+
+    def new(self, obj):
+        '''sets in __objects the obj with key <obj class name>.id'''
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        '''serializes __objects to the JSON file (path: __file_path)'''
+        ser_obj = {}
+        for key, obj in self.__objects.items():
+            ser_obj[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(ser_obj, file)
+
+    def reload(self):
+        ''' 
+        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; 
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+        '''
+        if isfile(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    cls_name = key.split('.')
+                    cls_obj = globals()[cls_name]
+                    obj_instance = cls_obj(**value)
+                    self.__objects[key] = obj_instance
